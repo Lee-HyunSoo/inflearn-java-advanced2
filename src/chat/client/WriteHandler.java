c894e3265710b99e16754e64eccc63b7eb6326f1
Lee-HyunSoo <cris_lx@naver.com>
2025-11-10 14:38:13 +0900

    채팅 프로그램

---
+++ src/chat/client/WriteHandler.java
@@ -0,0 +1,81 @@
+package chat.client;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import static util.MyLogger.log;
+
+public class WriteHandler implements Runnable {
+
+    private static final String DELIMITER = "|";
+
+    private final DataOutputStream output;
+    private final Client client;
+    private boolean closed;
+
+    public WriteHandler(DataOutputStream output, Client client) {
+        this.output = output;
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String username = inputUsername(scanner); // 블로킹
+            output.writeUTF("/join" + DELIMITER + username);
+
+            while (true) {
+                String toSend = scanner.nextLine();
+                if (toSend.isEmpty()) {
+                    // 아무것도 입력 안하고 엔터만 치는 경우
+                    continue;
+                }
+
+                if (toSend.equals("/exit")) {
+                    output.writeUTF(toSend);
+                    break;
+                }
+
+                // "/"로 시작하면 명령어, 나머지는 일반 메세지
+                if (toSend.startsWith("/")) {
+                    output.writeUTF(toSend);
+                } else {
+                    output.writeUTF("/message" + DELIMITER + toSend);
+                }
+            }
+        } catch (IOException | NoSuchElementException e) {
+            // Scanner의 System.in이 닫히면 NoSuchElementException이 터진다.
+            log(e);
+        } finally {
+            client.close();
+        }
+    }
+
+    private static String inputUsername(Scanner scanner) {
+        System.out.println("이름을 입력하세요.");
+        String username;
+        do {
+            username = scanner.nextLine();
+        } while (username.isEmpty());
+        return username;
+    }
+
+    public synchronized void close() {
+        if (closed) {
+            return;
+        }
+
+        try {
+            // Scanner를 닫는다. (사용자 입력 중지)
+            // Scanner는 블로킹되기 때문에 외부에서 해당 스레드를 종료 시키려면 System.in을 닫아야한다.
+            System.in.close();
+        } catch (IOException e) {
+            log(e);
+        }
+        closed = true;
+        log("writeHandler 종료");
+    }
+}
